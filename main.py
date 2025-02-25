f61e3ffcf509f8897651f495106e7020b55d5aab
evggas <evgeniyagas2000@gmail.com>
2025-02-25 19:34:40 +0300

    class

--- main.py
+++ main.py
@@ -23,6 +23,38 @@ button_exchange_rates = KeyboardButton(text="Курс валют")
 button_tips = KeyboardButton(text="Советы по экономии")
 button_finances = KeyboardButton(text="Личные финансы")
 
+keyboard = ReplyKeyboardMarkup(keyboard=[
+    [button_registr, button_exchange_rates],
+    [button_tips, button_finances]
+    ], resize_keyboard=True)
+
+conn = sqlite3.connect('user.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    telegram_id INTEGER UNIQUE,
+    name TEXT,
+    category1 TEXT,
+    category2 TEXT,
+    category3 TEXT,
+    expenses1 REAL,
+    expenses2 REAL,
+    expenses3 REAL
+    )
+''')
+
+conn.commit()
+
+class FinancesForm(StatesGroup):
+    category1 = State()
+    expenses1 = State()
+    category2 = State()
+    expenses2 = State()
+    category3 = State()
+    expenses3 = State()
+
 async def main():
 await dp.start_polling(bot)
 
